59e9eec9dd1774c84aa1217eee9daeb2744f6240
eldad mozes <eldad86.m@gmail.com>
2023-06-21 19:55:40 +0300

    fisrt commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+module "ec2" {
+  source = "./ec2"
+}
+
+module "s3" {
+  source = "./s3"
+}
+
+module "iam" {
+  source = "./iam"
+}
+
+module "dynamodb" {
+  source = "./dynamodb"
+}
+
+
+module "github" {
+  source = "./github"
+
+}
