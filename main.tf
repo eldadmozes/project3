0cef95a3e70e6af5b7e2166b23e31ce3dbd15dec
eldad mozes <eldad86.m@gmail.com>
2023-06-24 15:10:54 +0300

    1

--- main.tf
+++ main.tf
@@ -6,20 +6,20 @@ module "ec2" {
   source = "./ec2"
 }
 
-module "s3" {
-  source = "./s3"
-}
+# module "s3" {
+#   source = "./s3"
+# }
 
-module "iam" {
-  source = "./iam"
-}
+# module "iam" {
+#   source = "./iam"
+# }
 
-module "dynamodb" {
-  source = "./dynamodb"
-}
+# module "dynamodb" {
+#   source = "./dynamodb"
+# }
 
 
-module "github" {
-  source = "./github"
+# module "github" {
+#   source = "./github"
 
-}
+# }
