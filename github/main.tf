0b742225a0cf679caaf96682661cfbd5213e9265
eldad mozes <eldad86.m@gmail.com>
2023-06-25 20:04:58 +0300

    changes

--- github/main.tf
+++ github/main.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 }
+
 terraform {
   required_providers {
     github = {
@@ -10,17 +11,23 @@ terraform {
   }
 }
 
+variable "GITHUB_TOKEN" {
+  type    = string
+  default = ""
+}
+
 provider "github" {
   token = var.GITHUB_TOKEN
 }
+
 resource "github_repository" "project3-repo" {
   name        = "project3-repo"
-  description = "This repository created by terraform"
+  description = "This repository created by Terraform"
   visibility  = "public"
   auto_init   = true
 }
 
 resource "github_branch_default" "default_main" {
-  repository = "project3-repo"
+  repository = github_repository.project3-repo.name
   branch     = "main"
 }
