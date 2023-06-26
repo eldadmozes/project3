1683169b791ff6dd4e7e5bf4a718397f71f8c1ee
eldad mozes <eldad86.m@gmail.com>
2023-06-26 19:19:26 +0300

    changes

--- github/main.tf
+++ github/main.tf
@@ -1,3 +1,37 @@
+# provider "aws" {
+#   region = "us-east-1"
+# }
+
+# terraform {
+#   required_providers {
+#     github = {
+#       source  = "integrations/github"
+#       version = "~> 5.0"
+#     }
+#   }
+# }
+
+# variable "GITHUB_TOKEN" {
+#   type    = string
+# }
+
+# provider "github" {
+#   token = var.GITHUB_TOKEN
+# }
+
+# resource "github_repository" "project3-repo" {
+#   name        = "project3-repo"
+#   description = "This repository created by Terraform"
+#   visibility  = "public"
+#   auto_init   = true
+# }
+
+# resource "github_branch_default" "default_main" {
+#   repository = github_repository.project3-repo.name
+#   branch     = "main"
+# }
+
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -12,21 +46,16 @@ terraform {
 }
 
 variable "GITHUB_TOKEN" {
-  type    = string
+  type = string
 }
 
 provider "github" {
   token = var.GITHUB_TOKEN
 }
 
-resource "github_repository" "project3-repo" {
-  name        = "project3-repo"
-  description = "This repository created by Terraform"
-  visibility  = "public"
-  auto_init   = true
-}
-
-resource "github_branch_default" "default_main" {
-  repository = github_repository.project3-repo.name
-  branch     = "main"
+module "github" {
+  source        = "integrations/github/github"
+  version       = "~> 5.0"
+  GITHUB_TOKEN  = var.GITHUB_TOKEN
+  repository_name = "project3-repo"
 }
