fb84d0ccbca7e33d31ed7547bdf162060a05882e
eldad mozes <eldad86.m@gmail.com>
2023-06-26 19:28:15 +0300

    changes

--- github/main.tf
+++ github/main.tf
@@ -1,41 +1,6 @@
-# provider "aws" {
-#   region = "us-east-1"
-# }
-
-# terraform {
-#   required_providers {
-#     github = {
-#       source  = "integrations/github"
-#       version = "~> 5.0"
-#     }
-#   }
-# }
-
-# variable "GITHUB_TOKEN" {
-#   type    = string
-# }
-
-# provider "github" {
-#   token = var.GITHUB_TOKEN
-# }
-
-# resource "github_repository" "project3-repo" {
-#   name        = "project3-repo"
-#   description = "This repository created by Terraform"
-#   visibility  = "public"
-#   auto_init   = true
-# }
-
-# resource "github_branch_default" "default_main" {
-#   repository = github_repository.project3-repo.name
-#   branch     = "main"
-# }
-
-
 provider "aws" {
   region = "us-east-1"
 }
-
 terraform {
   required_providers {
     github = {
@@ -45,17 +10,17 @@ terraform {
   }
 }
 
-variable "GITHUB_TOKEN" {
-  type = string
-}
-
 provider "github" {
   token = var.GITHUB_TOKEN
 }
+resource "github_repository" "project3-repo" {
+  name        = "project3_repo"
+  description = "This repository created by terraform"
+  visibility  = "public"
+  auto_init   = true
+}
 
-module "github" {
-  source        = "./github"
-  version       = "~> 5.0"
-  GITHUB_TOKEN  = var.GITHUB_TOKEN
-  repository_name = "project3-repo"
+resource "github_branch_default" "project3-main_br" {
+  repository = "Terraform-project"
+  branch     = "main"
 }
